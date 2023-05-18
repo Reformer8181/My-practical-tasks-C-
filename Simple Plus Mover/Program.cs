7f1de7cf82a611c958d2a27f68659a22a6030fca
Strongban <strongban@gmail.com>
2023-05-18 21:41:48 +0300

    add Simple_Plus_Mover

---
+++ Simple Plus Mover/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Simple_Plus_Mover
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            ConsoleKeyInfo keyInfo;
+            do
+            {
+                Console.Clear();
+                for (int i = 0; i < position; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("+");
+                keyInfo = Console.ReadKey();
+                if (keyInfo.Key == ConsoleKey.LeftArrow) 
+                {
+                    position--;
+                }
+                else if (keyInfo.Key == ConsoleKey.RightArrow)
+                {
+                    position++;
+                }
+            }while (keyInfo.Key != ConsoleKey.Escape);
+        }
+    }
+}
