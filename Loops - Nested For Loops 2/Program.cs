481fad84a64458f8734f30a3b104751691028f58
Strongban <strongban@gmail.com>
2023-05-19 19:55:02 +0300

    add ne loops - nested for loops 2 example

---
+++ Loops - Nested For Loops 2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Loops___Nested_For_Loops_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // nested loops = loops inside of other loops
+            //                Uses vary. Used a lot in sorting algorithms.
+
+            Console.Write("How many rows?: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How many colums?: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What symbol: ");
+            String symbol = Console.ReadLine();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
