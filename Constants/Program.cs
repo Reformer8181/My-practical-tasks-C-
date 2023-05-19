12a1658362d47ca3540cea613e938ae7784c9cc3
Strongban <strongban@gmail.com>
2023-05-19 17:54:51 +0300

    add Constants add Variables 2

---
+++ Constants/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Constants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* constants    = immutable values which are know at compile time
+             *                and do not change for the life of the program
+             */
+
+            const double PiValue = 3.14159;
+            const int TheAnswer = 42;
+
+            /* In C#, the naming convention for constants is to use Pascal case. 
+             * This means that the first letter of each word in the constant’s name should be capitalized, 
+             * and there should be no spaces between words.
+             */
+        }
+    }
+}
---
+++ Variables 2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Variables_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;                  // declaration
+            x = 123;                // initialization
+
+            int y = 321;            // declaration + initialization
+
+            int z = x + y;
+
+            int age = 21;           // whole integer
+            double height = 300.5;  // decimal number
+            bool alive = true;      // true or false
+            char symbol = '@';
+            String name = "Bro";
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Your age is " + age);
+            Console.WriteLine("You height is " + height + "cm");
+            Console.WriteLine("Are you alive? " + alive);
+            Console.WriteLine("Your symbol is " + symbol);
+
+            String userName = symbol + name;
+            Console.WriteLine("Your username is " + userName);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+/*
+ * In C#, `string` is an alias for the `System.String` class. 
+ * So, `string name = "Bro";` and `String name = "Bro";` are equivalent. 
+ * The only difference is that `String` requires the `System` namespace to be specified 
+ * or imported, while `string` does not. 
+ * It's a matter of style and convention which one you use.
+ */
