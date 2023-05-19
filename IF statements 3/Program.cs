8ce63621219ab511eda8802ca961b244589ce5c9
Strongban <strongban@gmail.com>
2023-05-19 19:26:27 +0300

    add new IF statemants 3 example

---
+++ IF statements 3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace IF_statements_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name: ");
+            String name = Console.ReadLine();
+
+            if (name != "" ) 
+            {
+                Console.WriteLine("Hello " + name);
+            }
+            else
+            {
+                Console.WriteLine("You did not enter your name!");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
