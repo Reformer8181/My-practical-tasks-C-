f445d32171a64cde33daa77dd14500a61441347a
Yury Strongban <strongban@gmail.com>
2023-06-01 13:27:46 +0300

    add string interpolation example

---
+++ String Interpolation/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace String_Interpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* string interpolation = allows us to insert variables into a string literal
+             *                      precede a string literal with $
+             *                      {} are placeholders
+             */
+
+            String firstName = "Bro";
+            String lastName = "Code";
+            int age = 21;
+
+            Console.WriteLine($"Hello {firstName} {lastName}.");
+            Console.WriteLine($"You are {age, -10} years old.");
+
+            Console.ReadKey();
+        }
+    }
+}
