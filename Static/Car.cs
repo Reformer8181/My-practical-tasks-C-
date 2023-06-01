fbd4061a70f2f8c7c3d77eae895ed25ccdfd6c86
Yury Strongban <strongban@gmail.com>
2023-06-01 16:52:05 +0300

    add static example

---
+++ Static/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    internal class Car
+    {
+        String model;
+        // public int numberOfCars;
+        public static int numberOfCars;
+
+        public Car(String model) 
+        {
+            this.model = model;
+            numberOfCars++;
+        }
+
+        public static void StartRace()
+        {
+            Console.WriteLine("The race has begun!");
+        }
+    }
+}
