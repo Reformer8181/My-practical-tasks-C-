efe23ca66708d30e820b94dd825cc4587e04074a
Yury Strongban <strongban@gmail.com>
2023-06-01 13:17:04 +0300

    add conditional operator

---
+++ conditional operator/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace conditional_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // conditional operator = used in conditional assignment if condition is true/false
+
+            // (condition) ? x : y
+
+            double temperature = 20;
+            String message;
+
+            message = (temperature >= 15) ? "It's warm outside!" : "It's cold outside!";
+
+            Console.WriteLine(message);
+
+            Console.ReadKey();
+        }
+    }
+}
