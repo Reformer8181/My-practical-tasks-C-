209444ded2a32f54ca7471ee352d2114ee2888b8
Yury Strongban <strongban@gmail.com>
2023-06-02 20:23:03 +0300

    add tostring method override example

---
+++ Classes - ToString Method/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Classes___ToString_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ToString() = converts an object to its string representation
+            //              so that it is suitable for display.
+
+            Car car = new Car("Chevy", "Corvette", 2022, "blue");
+
+            Console.WriteLine(car.ToString());
+
+            Console.WriteLine(car);
+
+            Console.ReadKey();
+        }
+    }
+    class Car
+    {
+        String make;
+        String model;
+        int year;
+        String color;
+
+        public Car(String make, String model, int year, String color)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+        }
+
+        public override string ToString()
+        {
+            String message = "This is a " + make + " " + model;
+            return message;
+        }
+    }
+}
