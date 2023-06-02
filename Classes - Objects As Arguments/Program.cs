1e93d82d18c5e7706c5f864256167674701e6404
Yury Strongban <strongban@gmail.com>
2023-06-02 19:42:29 +0300

    add objects as arguments example

---
+++ Classes - Objects As Arguments/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace Classes___Objects_As_Arguments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car("Mustang", "red");
+            Console.WriteLine(car1.color + " " + car1.model);
+
+            ChangeColor(car1, "black");
+
+            Car car2 = Copy(car1);
+            Console.WriteLine(car2.color + " " + car2.model);
+
+            Console.ReadKey();
+        }
+
+        public static void ChangeColor(Car car, String color)
+        {
+            car.color = color;
+        }
+
+        public static Car Copy(Car car)
+        {
+            return new Car(car.model,car.color);
+        }
+    }
+
+    class Car
+    {
+        public String model;
+        public String color;
+
+        public Car(String model, String color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+    }
+}
