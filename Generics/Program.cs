e324c22288a56168fee66ffb7fe5c606dfa26448
Yury Strongban <strongban@gmail.com>
2023-06-05 12:41:31 +0300

    add generic example

---
+++ Generics/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  generic =   "not specific to a particular data type"
+            //              add <T> to classes, methods, fields, etc.
+            //              allows for code reusabilyty for different dsts types
+
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            String[] stringArray = { "1", "2", "3", "4", "5" };
+
+            displayElements(intArray);
+            displayElements(doubleArray);
+            displayElements(stringArray);
+
+
+            Console.ReadKey();
+        }
+        public static void displayElements<T>(T[] array)
+        {
+            foreach (T element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
